9212debcd6943a8d422a7f9e4fdf0519f419a06f
Andressa Lombardoso <81827241+andielombardoso@users.noreply.github.com>
2023-03-11 15:44:19 -0300

    criando arquivo

---
+++ questao1.js
@@ -0,0 +1,11 @@
+let indice = 13;
+let soma = 0;
+
+    for (k = 0; k < indice; k++) {
+        soma = soma + k;
+        if (k >= indice){
+        break;
+        }
+    }
+    
+console.log(soma);
